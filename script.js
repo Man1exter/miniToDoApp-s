5b9a58153f376e623a023d4fd567f0471cc86929
Mariusz Perzynski <m.perzyk@interia.pl>
2020-10-25 12:30:51 +0100

    line - through to accept button r

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const $mainUl = document.querySelector("ul");
+const $liElement = document.querySelector("li");
 const $buttonAcc = document.querySelector(".a1");
 const $buttonDel = document.querySelector(".b1");
 
@@ -9,10 +10,10 @@ const deleteAll = (e) => {
     e.target.parentNode.remove()
 }
 
-const acceptAll = () => {
-    
+const acceptAll = (e) => {
+    e.target.parentNode.style.textDecoration = "line-through";
 }
 
 
-document.querySelectorAll("li button").forEach(ele => ele.addEventListener("click", deleteAll));
-document.querySelectorAll("li button").forEach(ele => ele.addEventListener("click", acceptAll));
+document.querySelectorAll("li button.b1").forEach(ele => ele.addEventListener("click", deleteAll));
+document.querySelectorAll("li button.a1").forEach(ele => ele.addEventListener("click", acceptAll));
