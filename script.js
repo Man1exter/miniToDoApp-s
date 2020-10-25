76bf07a1994c5dc4acb9d58b2875a97576de7d8a
Mariusz Perzynski <m.perzyk@interia.pl>
2020-10-25 12:23:32 +0100

    delete this elements on button parentNode

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 const $mainUl = document.querySelector("ul");
-const $liItem = document.querySelector("li");
-const buttonAcc = document.querySelector(".a1");
-const buttonDel = document.querySelector(".b1");
+const $buttonAcc = document.querySelector(".a1");
+const $buttonDel = document.querySelector(".b1");
+
+
+
+
+const deleteAll = (e) => {
+    e.target.parentNode.remove()
+}
+
+const acceptAll = () => {
+    
+}
+
+
+document.querySelectorAll("li button").forEach(ele => ele.addEventListener("click", deleteAll));
+document.querySelectorAll("li button").forEach(ele => ele.addEventListener("click", acceptAll));
