7f6c3367eff9fc05ba1457a3280d889d15f54130
Mariusz Perzynski <m.perzyk@interia.pl>
2020-10-25 12:11:41 +0100

    download buttons and ul / li to script . js

---
+++ script.js
@@ -0,0 +1,4 @@
+const $mainUl = document.querySelector("ul");
+const $liItem = document.querySelector("li");
+const buttonAcc = document.querySelector(".a1");
+const buttonDel = document.querySelector(".b1");
