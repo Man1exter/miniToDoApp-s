46212de343cf57128d5dff574bb6f3464aae2559
Mariusz Perzynski <m.perzyk@interia.pl>
2020-10-26 19:14:54 +0100

    add space in function to better view on code

--- script.js
+++ script.js
@@ -7,20 +7,24 @@ const $input = document.querySelector("input");
 
 
 const deleteAll = (e) => {
+    
     e.target.parentNode.remove()
 }
 
 const acceptAll = (e) => {
+
     e.target.parentNode.style.color = "gray";
     e.target.parentNode.style.fontSize = 10 + "px";
     e.target.parentNode.style.lineHeight = 60 + "px";
 }
 
 const searchTask = (e) => {
+
   searchItem = e.target.value.toLowerCase(); 
   let task = [...$liElement];
 
-  task.filter(li => li.textContent.toLowerCase().includes(searchItem));
+  task = task.filter(li => li.textContent.toLowerCase().includes(searchItem));
+
   $mainUl.textContent = '';
   task.forEach(li => ul.appendChild(li));
 };
